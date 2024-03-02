f25128db86fc011cdd69520ab01c972ec2c68016
Kushal Patel <kushal300603@gmail.com>
2024-03-02 12:53:42 +0530

    Driver and Constructer models added

--- server/src/models/driver.models.js
+++ server/src/models/driver.models.js
@@ -1,2 +1,34 @@
 import mongoose, {Schema} from "mongoose";
 
+const driverSchema = new Schema({
+    driverNumber:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    driverName:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    driverNationality:{
+        type: String,
+        required: true,
+    },
+    driverTeam:{
+        type: Schema.Types.ObjectId,
+        ref: 'Constructor',
+    },
+    driverPoints:[{
+        type: Number,
+        default: 0,
+    }],
+    driverPrice:{
+        type: Number,
+        default: 0,
+    },
+}, {timestamps: true});
+
+
+
+export default mongoose.model('Driver', driverSchema);
