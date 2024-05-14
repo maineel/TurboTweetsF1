84397e92a03284e93dfbf1c983ffc77d0b40f343
neel <neelxdxd@gmail.com>
2024-05-14 12:39:45 +0530

    minor updates & performance fixes

--- server/src/controllers/constructor.controller.js
+++ server/src/controllers/constructor.controller.js
@@ -3,10 +3,10 @@ import { Constructor } from "../models/constructor.model.js";
 import axios from "axios";
 
 const constructorDetails = asyncHandler(async (req, res) => {
-  const currentDetails = await axios.get(
-    "http://localhost:8000/api/v1/admin/getDriverStandings"
-  );
-  // console.log(currentDetails);
+  // const currentDetails = await axios.get(
+  //   "http://localhost:8000/api/v1/admin/getDriverStandings"
+  // );
+  // // console.log(currentDetails);
 
   const constructor = await Constructor.find().select(
     "-_id -__v -createdAt -updatedAt -constructorPrice -constructorDrivers -constructorPoints"
