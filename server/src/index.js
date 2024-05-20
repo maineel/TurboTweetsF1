3bafec7156a0cb68d9d162ad9a8a2ded38f37139
neel <neelxdxd@gmail.com>
2024-05-20 20:15:51 +0530

    changes to repo

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,27 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
-dotenv.config({path: '../.env'}); // configuring .env
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+dotenv.config({ path: "../.env" });
 
 connectDB()
-.then(() => {   // if successful connection established then start the PORT
-    app.listen(process.env.PORT || 8080, () => {
-        console.log("Server is running on Port: ", process.env.PORT);
+  .then(() => {
+    const server = createServer(app);
+    const io = new Server(server);
+    // console.log(server,io);
+    io.on("connection", (socket) => {
+      console.log("Socket Connected: ", socket.id);
+      socket.on("disconnect", () => {
+        console.log("user disconnected");
+      });
     });
-})
-.catch((error) => { // Error
+
+    app.listen(process.env.PORT || 8080, () => {
+      console.log("Server is running on Port: ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
     console.log("Connection Failed!!  ", error);
-});
+  });
