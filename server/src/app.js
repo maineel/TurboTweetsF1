566588bd76628db0c3b393e5d9773a7d2f5bfe04
neel <neelxdxd@gmail.com>
2024-05-25 17:25:38 +0530

    logut functionality testing on render

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(cors({origin: process.env.CORS_ORIGIN || "*"}));
+app.use(cors({origin: "http://localhost:5173" , credentials: true})); // to allow the request from different origin
 app.use(express.json({limit: "16kb"})); // to limit the size of data which express should accept
 app.use(express.urlencoded({extended: true, limit: "16kb"})) // with extended flag, we will get the nested data at deep level
 app.use(express.static("public")); // it is a addres folder, where we will keep files which we want to keep in public
