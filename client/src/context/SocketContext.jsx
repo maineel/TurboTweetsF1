3de5a338645a30f384c324c7a20cd503a1f2cc62
neel <neelxdxd@gmail.com>
2024-07-01 19:37:50 +0530

    APIs changed to prod server

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (user) {
-      const socket = io("http://localhost:8000", {
+      const socket = io("https://turbotweetsf1.onrender.com", {
         query: {
           userId: user._id,
         },
